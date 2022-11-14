f0d78edcf4d5ba59d068dbdbe9bb65743ec3565f
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-11-14 16:10:44 -0700

    Turned frame_extraction into a function

--- frame_extraction.py
+++ frame_extraction.py
@@ -1,6 +1,10 @@
+__all__ = ["extract_frames"]
+
 import os
 
 import cv2
+
+from utils import get_verbose_print
 
 OUTPUT_PATH = "data/"  # Path to directory of output images
 OUTPUT_FILE_NAME = "IMG_"  # Name of resulting images before frame number
@@ -8,33 +12,51 @@ OUTPUT_EXT = ".jpg"  # File extension of saved image
 SAVE_NTH = 1  # Save every nth image, increase for less images
 OUTPUT_NUM_DIGITS = 4  # Number of numberical digits for file name formatting
 
-# Get the input video
-video_path = input("Enter the path to the video to extract frames from: ")
-video = cv2.VideoCapture(video_path)
-if not video.isOpened():
-    print("Unable to open video.")
-    exit()
 
-if not os.path.exists(OUTPUT_PATH):
-    os.makedirs(OUTPUT_PATH)
+def extract_frames(
+    video_path: str,
+    output_path: str = OUTPUT_PATH,
+    output_file_name: str = OUTPUT_FILE_NAME,
+    output_ext: str = OUTPUT_EXT,
+    save_nth: int = SAVE_NTH,
+    output_num_digits: int = OUTPUT_NUM_DIGITS,
+    verbose: bool = True,
+):
+    verbose_print = get_verbose_print(verbose)
 
-# Read frames and write it to a file
-frame_num = 0
-while video.isOpened():
-    ret, frame = video.read()
+    # Get the input video
+    video = cv2.VideoCapture(video_path)
+    if not video.isOpened():
+        raise FileNotFoundError(f"Unable to open video at {video_path}.")
 
-    # Frames were being read flipped so flip it back
-    frame = cv2.flip(frame, 0)
+    if not output_path.endswith(os.path.sep):
+        output_path += os.path.sep
 
-    # break if video ends
-    if not ret:
-        break
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
-    if frame_num % SAVE_NTH == 0:
-        cv2.imwrite(
-            f"{OUTPUT_PATH}{OUTPUT_FILE_NAME}{frame_num:>0{OUTPUT_NUM_DIGITS}d}{OUTPUT_EXT}",
-            frame,
-        )
-    frame_num += 1
-video.release()
-print(f"Final frame number {frame_num - 1}")
+    # Read frames and write it to a file
+    frame_num = 0
+    while video.isOpened():
+        ret, frame = video.read()
+
+        # Frames were being read flipped so flip it back
+        frame = cv2.flip(frame, 0)
+
+        # break if video ends
+        if not ret:
+            break
+
+        if frame_num % save_nth == 0:
+            cv2.imwrite(
+                f"{output_path}{output_file_name}{frame_num:>0{output_num_digits}d}{output_ext}",
+                frame,
+            )
+        frame_num += 1
+    video.release()
+    verbose_print(f"Final frame number {frame_num - 1}")
+
+
+if __name__ == "__main__":
+    video_path = input("Enter the path to the video to extract frames from: ")
+    extract_frames(video_path)
