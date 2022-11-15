126ec6317d6617910ef052185f97ffa630726b6a
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-11-14 18:32:38 -0700

    Added ImageDataset ability to move images to device

--- utils/image_dataset.py
+++ utils/image_dataset.py
@@ -4,13 +4,17 @@ import torchvision
 
 
 class ImageDataset(torch.utils.data.Dataset):
-    def __init__(self, files: pd.DataFrame, transform=None):
+    def __init__(self, files: pd.DataFrame, transform=None, device=None):
         super().__init__()
         self.files = files.reset_index()
-        assert {"img_path", "class"}.issubset(self.files.columns), (
-            "files must have columns 'img_path' and 'class'"
-        )
+        assert {"img_path", "class"}.issubset(
+            self.files.columns
+        ), "files must have columns 'img_path' and 'class'"
         self.transform = transform
+        if device is None:
+            device = torch.device("cpu")
+            print("No device specified, using CPU")
+        self.device = device
 
     def __len__(self):
         return len(self.files)
@@ -18,6 +22,7 @@ class ImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         img_path, label = self.files.loc[index, ["img_path", "class"]]
         image = torchvision.io.read_image(img_path)
+        image = image.to(self.device)
         if self.transform is not None:
             image = self.transform(image)
-        return image, label
+        return image, label.to(self.device)
