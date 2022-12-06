fc457ab929d10e1a136fdc0fd7368255275f9323
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-12-06 10:41:45 -0700

    Changed move_files to use threads

--- move_files.py
+++ move_files.py
@@ -1,20 +1,22 @@
 import torchvision
 from tqdm.auto import tqdm
+from tqdm.contrib.concurrent import thread_map
 
 import create_apple_model as cam
 
 new_path = "D:\\temp\\haar_cascade\\"
 
-df_files = cam.get_df_files()
-df_files = df_files[df_files["class"] != cam.Categories.APPLE.value].reset_index(
-    drop=True
-)
-cropper = cam.CropToSquare()
-for index in tqdm(range(len(df_files))):
-    row = df_files.loc[index]
+def save_image(row):
+    index = row[0]
+    row = row[1]
+    cropper = cam.CropToSquare()
     img_path = row["img_path"]
     image = torchvision.io.read_image(img_path)
     class_ = "p" if row["class"] == cam.Categories.APPLE.value else "n"
     if class_ == "p":
         image = cropper(image)
-    torchvision.io.write_jpeg(image, new_path + class_ + "\\" + str(index) + ".jpg")
+    torchvision.io.write_jpeg(image,
+                              new_path + class_ + "\\" + str(index) + ".jpg")
+
+df_files = cam.get_df_files()
+thread_map(save_image, list(df_files.iterrows()), max_workers=8)
